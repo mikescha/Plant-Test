f4151a2905ee84fe24ac589c5fc107cadd025761
Schack <mikescha@hotmail.com>
2017-03-06 11:03:05 -0800

    Add project files.

---
+++ Plant Test/Plant_Test/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Plant_Test
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
