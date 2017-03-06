7a13903b19d20683cf84386b76c5e636e1139d18
Schack <mikescha@hotmail.com>
2017-03-06 11:18:53 -0800

    Added NuGet packages

---
+++ Plant Test/Plant_Test/Data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plant_Test
+{
+    public class Data
+    {
+    }
+
+    public interface IDatabaseConnection
+    {
+        SQLite.SQLiteConnection DbConnection();
+    }
+
+}
